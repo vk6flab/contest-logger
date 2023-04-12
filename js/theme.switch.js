be22c658007191be7b59e13180f8f25f048ec8bd
vk6flab <cq@vk6flab.com>
2023-04-12 03:59:41 +0000

    buid: moved application into the public directory

---
+++ js/theme.switch.js
@@ -0,0 +1,41 @@
+/*
+	Based on: https://www.thesitewizard.com/javascripts/change-style-sheets.shtml
+*/
+
+function switch_style ( css_title ) {
+	Array.from(document.getElementsByTagName("link")).forEach(
+		(link_tag) => {
+			if ((link_tag.rel.indexOf( "stylesheet" ) != -1) && link_tag.title) {
+				link_tag.disabled = true ;
+				if (link_tag.title == css_title) {
+					link_tag.disabled = false ;
+				}
+			}
+			set_pref( 'style', css_title );
+		}
+	);
+}
+
+function set_style_from_pref() {
+	var css_title = get_pref( 'style' );
+	if (css_title !== null) {
+		switch_style( css_title );
+		const menu = document.getElementById ('theme');
+		menu.selectedIndex = [...menu.options].findIndex (option => option.text === css_title);
+	}
+}
+
+function set_pref ( name, value ) {
+	window.localStorage.setItem( name, JSON.stringify(value)) ;
+}
+
+function get_pref ( name ) {
+	return JSON.parse(window.localStorage.getItem(name)) ;
+}
+
+function switchTheme(event) {
+	var newTheme = this.options[this.selectedIndex].text;
+	switch_style (newTheme) ;
+}
+
+set_style_from_pref();
