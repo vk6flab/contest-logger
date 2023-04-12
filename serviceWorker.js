770f97ac984f4f3ebf2c6f2458634c7d58814a6b
vk6flab <cq@vk6flab.com>
2023-04-12 05:17:42 +0000

    build: remove absolute paths

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,26 +5,26 @@ self.addEventListener("install", (event) => {
 		caches.open(cacheName).then((cache) => {
 			return cache.addAll([
 				//fixme: This should not have to be hard coded and will break as soon as we add new files
-				"/",
-				"/index.html",
-				"/manifest.json",
-				"/js/commons.min.js",
-				"/js/theme.switch.js",
-				"/js/modal.js",
-				"/js/main.js",
-				"/css/pico.min.css",
-				"/css/horizontal.css",
-				"/css/pico.min.css.map",
-				"/css/style.css",
-				"/css/pico.css",
-				"/css/pico.css.map",
-				"/css/pico.docs.min.css",
-				"/icons/favicon.ico",
-				"/icons/favicon-16x16.png",
-				"/icons/favicon-32x32.png",
-				"/icons/android-chrome-512x512.png",
-				"/icons/android-chrome-192x192.png",
-				"/icons/apple-touch-icon.png"
+				"",
+				"index.html",
+				"manifest.json",
+				"js/commons.min.js",
+				"js/theme.switch.js",
+				"js/modal.js",
+				"js/main.js",
+				"css/pico.min.css",
+				"css/horizontal.css",
+				"css/pico.min.css.map",
+				"css/style.css",
+				"css/pico.css",
+				"css/pico.css.map",
+				"css/pico.docs.min.css",
+				"icons/favicon.ico",
+				"icons/favicon-16x16.png",
+				"icons/favicon-32x32.png",
+				"icons/android-chrome-512x512.png",
+				"icons/android-chrome-192x192.png",
+				"icons/apple-touch-icon.png"
 			]);
 		})
 	);
